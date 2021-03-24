39cf7415da8cd659c8aad0322dff169ed3073fbb
sianjin <47802717+sianjin@users.noreply.github.com>
2021-03-24 13:51:10 -0700

    Delete 1 SU simulation code directory

--- 1 SU simulation code/3 log-SGN method/absAvgPerVsTxSnr.m
+++
@@ -1,3 +0,0 @@
-perFastAbsVec = [0.8462, 0.4267, 0.1127, 0.0103];
-snrs = [11,15,19,23];
-plot(snrs, perFastAbsVec)
--- 1 SU simulation code/3 log-SGN method/skewGeneralizedNormal.m
+++
@@ -1,4 +0,0 @@
-function X = skewGeneralizedNormal(epsilo, omega, lamdbda1, lambda2)
-alpha = sqrt(lambda2) * normrnd(0,1) + lamdbda1;
-X = skewNormal(epsilo, omega, alpha);
-end
